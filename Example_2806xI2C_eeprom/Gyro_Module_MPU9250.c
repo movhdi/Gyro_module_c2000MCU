d87465592d3121c6fb44c4ddc021f2d0934559b4
mmova <m.movahedimonfared@gmail.com>
2022-03-27 22:36:51 +0430

    the licence texts of the gyro .c file corrected.

--- Example_2806xI2C_eeprom/Gyro_Module_MPU9250.c
+++ Example_2806xI2C_eeprom/Gyro_Module_MPU9250.c
@@ -1,8 +1,14 @@
-/*
- * Gyro_Module_MPU9250.c
- *
- *  Created on: Mar 26, 2022
- *      Author: mmova
- */
+//###########################################################################
+//
+// FILE:    Gyro_Module_MPU9250.c
+//
+// TITLE:   Gyroscope and Accelerometer and Magnetometer Code Definitions.
+//
+//###########################################################################
+// $Release Date: 2022 $
+// $Copyright:
+// Copyright (C) 2022 Mohammad Mahdi Movahedi Monfared
+//
+//###########################################################################
 
 #include "Gyro_Module_MPU9250.h"
