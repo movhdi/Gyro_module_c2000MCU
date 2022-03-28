ff3fcdeb3a0c578baf4c0245ba95a415c16fd06f
mmova <m.movahedimonfared@gmail.com>
2022-03-28 15:50:33 +0430

    I2CMSG structure defined in a new file as I2C_MPU9250.h

---
+++ Example_2806xI2C_eeprom/I2C_MPU9250.h
@@ -0,0 +1,49 @@
+/*
+ * I2C_MPU9250.h
+ *
+ *  Created on: Mar 28, 2022
+ *      Author: mmova
+ */
+
+#ifndef I2C_MPU9250_H_
+#define I2C_MPU9250_H_
+
+//
+// I2C Message Structure
+//
+struct I2CMSG {
+    //
+    // Word stating what state msg is in:
+    //   I2C_MSGCMD_INACTIVE = do not send msg
+    //   I2C_MSGCMD_BUSY = msg start has been sent,
+    //                     awaiting stop
+    //   I2C_MSGCMD_SEND_WITHSTOP = command to send
+    //       master trans msg complete with a stop bit
+    //   I2C_MSGCMD_SEND_NOSTOP = command to send
+    //       master trans msg without the stop bit
+    //   I2C_MSGCMD_RESTART = command to send a restart
+    //       as a master receiver with a stop bit
+    //
+    Uint16 MsgStatus;
+
+    //
+    // I2C address of slave msg is intended for
+    //
+    Uint16 SlaveAddress;
+
+    //
+    // Register address of MPU9250
+    //
+    Uint16 RegisterAdress;
+
+    //
+    // Array holding msg data - max that MAX_BUFFER_SIZE can be is 4 due to
+    // the FIFO's
+    //
+    Uint16 MsgBuffer[I2C_MAX_BUFFER_SIZE];
+};
+
+
+
+
+#endif /* I2C_MPU9250_H_ */
